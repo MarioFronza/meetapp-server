9a1b0657e03397a6b6b8545f032495783dd20fbd
Mario <mariofronza1@gmail.com>
2019-08-15 08:24:12 -0300

    meetup management

--- src/database/index.js
+++ src/database/index.js
@@ -2,20 +2,30 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Meetup];
 
 class Database {
   constructor() {
     this.init();
+    this.associate();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
   }
 }
 
